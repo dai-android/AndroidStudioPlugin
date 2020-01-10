642f9701914dfff6c954e8328d00792f037b5d96
patrick <patrick@dai>
2020-01-10 17:37:29 +0800

    Demo: try deal AndroidMenifest file: just begin

--- media-plugin/src/main/groovy/dai/android/plugin/MediaPlugin.groovy
+++ media-plugin/src/main/groovy/dai/android/plugin/MediaPlugin.groovy
@@ -9,10 +9,26 @@ class MediaPlugin implements Plugin<Project> {
 
     public static final String DISPATCHER_EXTENSION_NAME = "dispatcher"
 
+    private String rootDirPath
+    private String packageName
+
+    private static def getPackageName(variant) {
+        if (null == variant) {
+            return null
+        }
+
+        [variant.mergedFlavor.applicationId, variant.buildType.applicationIdSuffix].findAll().join()
+    }
+
+
     @Override
     void apply(Project project) {
 
         println "<>: Plugin apply begin"
+
+        rootDirPath = project.rootDir.absolutePath
+        println "<>: root dir path: " + rootDirPath
+
 
         project.extensions.create(DISPATCHER_EXTENSION_NAME, DispatcherExtension)
 
@@ -21,6 +37,23 @@ class MediaPlugin implements Plugin<Project> {
         // register Transform
         def classTransform = new MediaTransform(project)
         android.registerTransform(classTransform)
+
+
+        project.afterEvaluate {
+            android.applicationVariants.all { variant ->
+                if (packageName == null) {
+                    packageName = getPackageName(variant)
+                    println "<>: package name: " + packageName
+                }
+
+                variant.outputs.each { output ->
+                    println  output
+                    //output.processManifestProvider.get().doLast {
+                    //    println "<>: manifestOutputDirectory:" + output.processManifestProvider.get().manifestOutputDirectory.absolutePath
+                    //}
+                }
+            }
+        }
 
         println "<>: Plugin apply end"
     }
